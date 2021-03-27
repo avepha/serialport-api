fa0366e152913b06ad7e468e618e679f170c85bb
Farhan Poh-Asae <farhan.abuhazan@gmail.com>
2021-03-27 23:57:52 +0700

    feat: receive data from serialport

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{io, thread, str};
+use serialport::{SerialPort};
+
 fn main() {
-    println!("Hello, world!");
+    let port_name = "/dev/cu.usbserial-DN05IJFB";
+    let port = serialport::new(port_name, 345600)
+        .open()
+        .expect("Failed to open serial port");
+
+    let listen_port_thread = thread::spawn(|| {
+        listen_port(port);
+    });
+
+    listen_port_thread.join().unwrap();
+}
+
+fn listen_port(mut port: Box<dyn SerialPort>) {
+    let mut buffer: [u8; 1] = [0; 1];
+    let mut bytes = Vec::new();
+    loop {
+        match port.read(&mut buffer) {
+            Ok(_length) => {
+                bytes.push(buffer[0]);
+                if bytes[bytes.len() - 1] == 13 {
+                    println!("{:?}", str::from_utf8(&bytes));
+                    bytes = Vec::new();
+                }
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+            Err(e) => eprintln!("{:?}", e),
+        }
+    }
 }
